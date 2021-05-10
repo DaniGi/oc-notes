3c75a0e82287f32fed6d7ae23263086a00deeeeb
Daniele Gioria <daniele1gioria@gmail.com>
2021-05-10 17:06:29 +0200

    added bookmark tooltip

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -1,5 +1,7 @@
 import * as React from 'react';
 import { createStyles, makeStyles, Theme, Typography } from '@material-ui/core';
+import IconButton from '@material-ui/core/IconButton';
+import Tooltip from '@material-ui/core/Tooltip';
 import BookmarkBorderIcon from '@material-ui/icons/BookmarkBorder';
 import BookmarkIcon from '@material-ui/icons/Bookmark';
 import blue from '@material-ui/core/colors/blue';
@@ -11,6 +13,8 @@ export interface ISection {
 
 type Props = ISection;
 
+const { useState } = React;
+
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     root: {
@@ -18,9 +22,13 @@ const useStyles = makeStyles((theme: Theme) =>
       position: 'relative',
     },
     bookmark: {
+      padding: 0,
       position: 'absolute',
       top: 3,
       left: -27,
+      '&:hover': {
+        backgroundColor: 'transparent',
+      },
     },
     bookmark__icon: {
       color: blue[200],
@@ -33,21 +41,23 @@ const useStyles = makeStyles((theme: Theme) =>
 
 const Section: React.FC<Props> = ({ title, content }) => {
   const classes = useStyles();
-  const [isHoverIcon, setIsHoverIcon] = React.useState(false);
+  const [isActiveIcon, setIsactiveIcon] = useState(false);
 
   return (
     <div className={classes.root}>
-      <div
-        className={classes.bookmark}
-        onMouseEnter={() => setIsHoverIcon(true)}
-        onMouseLeave={() => setIsHoverIcon(false)}
-      >
-        {isHoverIcon ? (
-          <BookmarkIcon className={classes.bookmark__icon__hover} />
-        ) : (
-          <BookmarkBorderIcon className={classes.bookmark__icon} />
-        )}
-      </div>
+      <Tooltip title={`${isActiveIcon ? 'Supprimer bookmark' : 'Ajouter bookmark'}`}>
+        <IconButton
+          aria-label="add bookmark"
+          className={classes.bookmark}
+          onClick={() => setIsactiveIcon((prev) => !prev)}
+        >
+          {isActiveIcon ? (
+            <BookmarkIcon className={classes.bookmark__icon__hover} />
+          ) : (
+            <BookmarkBorderIcon className={classes.bookmark__icon} />
+          )}
+        </IconButton>
+      </Tooltip>
       <Typography variant="h3">{title}</Typography>
       <Typography>{content}</Typography>
     </div>
