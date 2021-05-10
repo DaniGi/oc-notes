e43cde8657b8949535a37c535b9ccda9b8049116
Daniele Gioria <daniele1gioria@gmail.com>
2021-05-10 10:38:27 +0200

    added accordion and bookmark list

--- src/components/CoursChapter.tsx
+++ src/components/CoursChapter.tsx
@@ -1,5 +1,6 @@
-import { createStyles, Grid, makeStyles, Theme, Typography } from '@material-ui/core';
 import * as React from 'react';
+import { createStyles, Grid, makeStyles, Theme, Typography } from '@material-ui/core';
+import Accordion from './Accordion';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -16,6 +17,14 @@ const CoursChapter: React.FC = () => {
     <Grid container justify="center" className={classes.root}>
       <Grid item xs={12} sm={10}>
         <Typography variant="h2">Maîtrisez votre e-réputation</Typography>
+      </Grid>
+      <Grid item container xs={12} sm={10}>
+        <Grid item xs={12} sm={9} style={{ paddingRight: '30px' }}>
+          <Accordion />
+        </Grid>
+        <Grid item xs={12} sm={3}>
+          BookmarkList
+        </Grid>
       </Grid>
     </Grid>
   );
