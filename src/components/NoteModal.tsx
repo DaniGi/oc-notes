4c6f1e94d4fea0f3ab38fab04dc85fdbf2a98ae4
Daniele Gioria <daniele1gioria@gmail.com>
2021-05-12 10:09:11 +0200

    added dynamic dialog title

--- src/components/NoteModal.tsx
+++ src/components/NoteModal.tsx
@@ -3,9 +3,21 @@ import TextField from '@material-ui/core/TextField';
 import Dialog from '@material-ui/core/Dialog';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogTitle from '@material-ui/core/DialogTitle';
+import Grow from '@material-ui/core/Grow';
+import { TransitionProps } from '@material-ui/core/transitions';
 import { useNotes } from '../contexts/NotesContext';
 
-const { useState, useRef } = React;
+const { useState, useRef, useEffect } = React;
+
+const Transition = React.forwardRef(
+  (
+    // eslint-disable-next-line react/require-default-props
+    props: TransitionProps & { children?: React.ReactElement<any, any> },
+    ref: React.Ref<unknown>,
+  ) => {
+    return <Grow ref={ref} {...props} style={{ transformOrigin: '0 0 0' }} />;
+  },
+);
 
 interface Props {
   open: boolean;
@@ -17,15 +29,26 @@ const NoteModal: React.FC<Props> = ({ open, setOpen, id }) => {
   const { notesState, notesDispatch } = useNotes();
   const [input, setInput] = useState('');
   const inputRef = useRef<HTMLInputElement>(null);
+  const noteRef = useRef(false);
+
+  useEffect(() => {
+    const items = notesState.filter((note) => note.id === id);
+    if (items.length === 0) {
+      noteRef.current = false;
+    } else {
+      noteRef.current = true;
+    }
+  });
 
   const handleClose = () => {
+    setOpen(false);
+    if (input.length === 0) return;
     const notes = notesState.filter((note) => note.id === id);
     if (notes.length === 0) {
       notesDispatch({ type: 'add-note', payload: { id, content: input } });
     } else {
       notesDispatch({ type: 'mofify-note', payload: { id, content: input } });
     }
-    setOpen(false);
   };
 
   const handleOnChange = () => {
@@ -35,8 +58,15 @@ const NoteModal: React.FC<Props> = ({ open, setOpen, id }) => {
   };
 
   return (
-    <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
-      <DialogTitle id="form-dialog-title">Votre note</DialogTitle>
+    <Dialog
+      open={open}
+      TransitionComponent={Transition}
+      onClose={handleClose}
+      aria-labelledby="form-dialog-title"
+    >
+      <DialogTitle id="form-dialog-title">
+        {noteRef.current ? 'Modifier note' : 'Nouvelle note'}
+      </DialogTitle>
       <DialogContent>
         <TextField
           autoFocus
